8f8b2d8ca91f9e53dd425cd4635ed2eb02b37a47
YuDavidCao <yu.cao20041208@gmail.com>
2025-03-26 21:19:46 -0400

    added authContext for tracking current user's authentication information and ProtectedRoute for auth verification on routes.

--- Views/src/App.jsx
+++ Views/src/App.jsx
@@ -5,28 +5,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GraphPage from "./GraphPage";
 import SignIn from "./signin";
 import SignUp from "./signup";
+import { AuthProvider } from "./authContext";
 
 function App() {
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/upload"
-            element={
-              <div className="flex flex-col items-center justify-center w-screen h-screen">
-                <div className="flex flex-col items-center justify-center w-full max-w-lg">
-                  <DragAndDrop></DragAndDrop>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/upload"
+              element={
+                <div className="flex flex-col items-center justify-center w-screen h-screen">
+                  <div className="flex flex-col items-center justify-center w-full max-w-lg">
+                    <DragAndDrop></DragAndDrop>
+                  </div>
                 </div>
-              </div>
-            }
-          />
-          <Route path="/graph" element={<GraphPage></GraphPage>} />
-          <Route path="/sign-in" element={<SignIn></SignIn>} />
-          <Route path="/sign-up" element={<SignUp></SignUp>} />
-        </Routes>
-      </BrowserRouter>
+              }
+            />
+            <Route path="/graph" element={<GraphPage></GraphPage>} />
+            <Route path="/sign-in" element={<SignIn></SignIn>} />
+            <Route path="/sign-up" element={<SignUp></SignUp>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
