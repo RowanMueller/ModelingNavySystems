a254ecf09e5edb49acc480205aacc1fe085fd4fa
Saraa Kumar <saraakumar06@gmail.com>
2025-04-24 15:57:58 -0400

    Fix Docker + Django settings, restructure for Services

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import django
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Services.server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
